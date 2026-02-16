9c3b1ae36d41baa2ac6b1ac38c7045ec4743329c
nishrajpal05 <47219617+nishrajpal05@users.noreply.github.com>
2026-02-16 20:34:58 +0530

    Update index.js

--- research-digest/frontend/src/index.js
+++ research-digest/frontend/src/index.js
@@ -1,18 +1,9 @@
-// frontend/src/index.js
-
-/*
-This is the entry point - where React starts
-Think of it like the front door of your app
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/App.css';
 import App from './App';
 
-// Get the HTML element with id="root" from index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Render (show) our App inside that element
 root.render(
   <React.StrictMode>
     <App />
